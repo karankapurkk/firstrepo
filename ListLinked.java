64a305e133e26fc0bc26397c4ad9f36761b0bea6
Roma Dembla <yc03rd3@macys.com>
2015-10-26 09:55:36 +0530

    Link List Program

---
+++ ListLinked.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+
+public class ListLinked {
+
+	public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
+//		System.out.println(startTime);
+		LinkedList<Integer> ll = new LinkedList<Integer>();
+		for (int i = 0; i < 1000; i++)
+		{
+		   ll.add(i);
+		   System.out.print(ll.get(i) +" ");
+		}
+		long endTime=System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+		System.out.println();
+		System.out.println(totalTime);
+	}
+
+}
